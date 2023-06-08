d439ed21a77885bd6cd0d78aafeab34f4e77ed94
Mary <maryup.for.reg@gmail.com>
2023-06-08 12:32:50 +0300

    hw1 realisation first repository based on ConcurrentHashMap

---
+++ src/main/java/ru/javawebinar/topjava/repository/MemoryMealRepository.java
@@ -0,0 +1,38 @@
+package ru.javawebinar.topjava.repository;
+import ru.javawebinar.topjava.model.Meal;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+
+public class MemoryMealRepository implements MealsRepository {
+    private final AtomicInteger idCounter = new AtomicInteger(0);
+    private final Map<Integer, Meal> repository = new ConcurrentHashMap<>();
+
+    @Override
+    public Meal save(Meal meal) {
+        if (meal.isNew()) {
+            meal.setId(idCounter.incrementAndGet());
+            repository.put(meal.getId(), meal);
+            return meal;
+        }
+        return repository.computeIfPresent(meal.getId(), (id, oldMeal) -> meal);
+    }
+
+    @Override
+    public Meal get(int id) {
+        return repository.get(id);
+    }
+
+    @Override
+    public boolean delete(int id) {
+        return repository.remove(id) != null;
+    }
+
+    @Override
+    public Collection<Meal> getAll() {
+        return repository.values();
+    }
+}
